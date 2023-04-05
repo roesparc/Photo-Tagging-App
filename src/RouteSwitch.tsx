5639b864d598f9a8d2f8ee789af870de310ebe80
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-05 11:10:53 -0600

    Add Header component with props

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,10 +1,31 @@
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import styles from "./styles/RouteSwitch.module.scss";
+import { useState } from "react";
+import { FoundCharacters } from "./types";
+
+const INITIAL_FOUND_CHARACTERS = {
+  lenny: false,
+  flanders: false,
+  ralph: false,
+  skinner: false,
+  otto: false,
+};
 
 const RouteSwitch = () => {
+  const [foundCharacters, setFoundCharacters] = useState<FoundCharacters>(
+    INITIAL_FOUND_CHARACTERS
+  );
+
+  const handleFoundCharacters = (foundCharacter: string) => {
+    setFoundCharacters((prev) => ({ ...prev, [foundCharacter]: true }));
+  };
+
   return (
     <div className={styles.root}>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Header foundCharacters={foundCharacters} />
+      </BrowserRouter>
     </div>
   );
 };
