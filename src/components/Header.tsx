163a9acb009f67a4e02fa60989ec60d4eb2d9620
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-06 19:20:21 -0600

    Use image paths object to display header images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,7 @@
-import krusty from "../assets/img/characters/krusty.png";
-import burns from "../assets/img/characters/burns.png";
-import flanders from "../assets/img/characters/flanders.png";
-import apu from "../assets/img/characters/apu.png";
-import doctor from "../assets/img/characters/doctor.png";
-
 import { Link, useLocation } from "react-router-dom";
 import styles from "../styles/Header.module.scss";
-import { FoundCharacters } from "../types";
+import { FoundCharacters } from "../common/types";
+import imagePaths from "../assets/imagePaths";
 
 interface Props {
   foundCharacters: FoundCharacters;
@@ -30,27 +25,27 @@ const Header = ({ foundCharacters }: Props) => {
           <div className={styles.characters}>
             <img
               className={foundCharacters.burns ? styles.found : undefined}
-              src={burns}
+              src={imagePaths.burns}
               alt="Mr. Burns"
             />
             <img
               className={foundCharacters.flanders ? styles.found : undefined}
-              src={flanders}
+              src={imagePaths.flanders}
               alt="Flanders"
             />
             <img
               className={foundCharacters.krusty ? styles.found : undefined}
-              src={krusty}
+              src={imagePaths.krusty}
               alt="Krusty"
             />
             <img
               className={foundCharacters.apu ? styles.found : undefined}
-              src={apu}
+              src={imagePaths.apu}
               alt="Apu"
             />
             <img
               className={foundCharacters.doctor ? styles.found : undefined}
-              src={doctor}
+              src={imagePaths.doctor}
               alt="Dr. Hibbert"
             />
           </div>
