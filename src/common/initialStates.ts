227bb9e2e26c42b65baa1fa45c88927f0f57046a
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-04-06 19:16:36 -0600

    Create initial states file

---
+++ src/common/initialStates.ts
@@ -0,0 +1,22 @@
+export const INITIAL_FOUND_CHARACTERS = {
+  burns: false,
+  flanders: false,
+  krusty: false,
+  apu: false,
+  doctor: false,
+};
+
+const INITIAL_COORDINATES = {
+  x1: 0,
+  x2: 0,
+  y1: 0,
+  y2: 0,
+};
+
+export const INITIAL_CHARACTERS_LOCATION = {
+  burns: INITIAL_COORDINATES,
+  flanders: INITIAL_COORDINATES,
+  krusty: INITIAL_COORDINATES,
+  apu: INITIAL_COORDINATES,
+  doctor: INITIAL_COORDINATES,
+};
